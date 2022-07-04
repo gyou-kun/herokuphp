8d14aa32b671f09f12cb916c64f0bed5a04a1beb
gyou-kun <20024466@myrp.edu.sg>
2022-07-04 13:11:04 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,7 +1,13 @@
+<!DOCTYPE html>
 <html>
 <head>
+<title>Hello World</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
   Hello World!
+  <p lang="fr">Ceci est un paragraphe.</p>
+
 </body>
 </html>
