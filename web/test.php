0370ea26360c27fe2e130ba7736f458d003383f5
gyou-kun <20024466@myrp.edu.sg>
2022-07-04 13:20:34 +0800

    hi

--- web/test.php
+++ web/test.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>Hello World</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Put your description here.">
 
 </head>
 <body>
   Hello World!
-  <p lang="fr">Ceci est un paragraphe.</p>
 
 </body>
 </html>
